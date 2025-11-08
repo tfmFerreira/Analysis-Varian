6de0764fd25d3a54d03120885d40281b2a107795
Tiago M. Ferreira <ferreira.t.f.m@gmail.com>
2025-11-08 01:20:32 +0100

    Add files via upload

--- test-NMRglue.py
+++ test-NMRglue.py
@@ -3,20 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-dirname = os.getcwd()
+rootdir = os.getcwd()
 
-print(dirname)
+# The list (vector) of folder numbers you want to loop over
+folder_numbers = [40754, 40754, 43000, 43916, 44101, 44131, 44500, 46775, 50297]
 
-dic,data = ng.varian.read(dirname,"fid","procpar")
-
-A = ng.varian.create_data(data)
-
-Bf1=dic.get("procpar").get("H1reffrq").get("values")
-NP = float(dic.get("procpar").get("np").get("values")[0])
-SW = float(dic.get("procpar").get("sw").get("values")[0])
-DW = 1/(SW)
-ACQtime = np.arange(NP/2) * DW
-np.savetxt("output.txt", np.column_stack((ACQtime, A.real, A.imag)), fmt="%.6f")
-Bf1 = np.array(Bf1, dtype=float)   # convert everything to floats
-np.savetxt("Bf1.txt", Bf1, fmt="%.6f")
+for num in folder_numbers:
+	folder_name = f"20251031_13C_rinept_37C_DECpwr{num}.fid"
+	dirname = os.path.join(rootdir, folder_name)
+	dic,data = ng.varian.read(dirname,"fid","procpar")
+	A = ng.varian.create_data(data)
+	Bf1=dic.get("procpar").get("H1reffrq").get("values")
+	NP = float(dic.get("procpar").get("np").get("values")[0])
+	SW = float(dic.get("procpar").get("sw").get("values")[0])
+	DW = 1/(SW)
+	ACQtime = np.arange(NP/2) * DW
+	out_file = os.path.join(dirname, f"output.txt")
+	np.savetxt(out_file, np.column_stack((ACQtime, A.real, A.imag)), fmt="%.6f")
+	Bf1 = np.array(Bf1, dtype=float)   # convert everything to floats
+	out_file = os.path.join(dirname, f"Bf1.txt")
+	np.savetxt(out_file, Bf1, fmt="%.6f")
 
